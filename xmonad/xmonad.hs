4d7435a2a04077b1f7266b54f2f95d8c9e429e65
Clint Tseng <clint@dontexplain.com>
2013-05-22 21:59:19 -0700

    xmonad: add new layout options; bind change layout keys.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,8 +1,24 @@
+module Main (main) where
+
+import qualified Data.Map as M
+
 import XMonad
+
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.ToggleLayouts
+
+layouts = toggleLayouts Full ((Tall 1 0.03 0.5) ||| (ThreeCol 1 0.03 0.5) ||| (ThreeColMid 1 0.03 0.5))
+
+customKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList
+    [ ((modm .|. shiftMask, xK_BackSpace), sendMessage ToggleLayout)
+    , ((modm, xK_BackSpace), sendMessage NextLayout) ]
 
 main :: IO ()
 main = xmonad $ defaultConfig
     { borderWidth = 1
     , terminal = "/usr/local/bin/urxvtc"
     , normalBorderColor = "#000000"
-    , focusedBorderColor = "#555555" }
+    , focusedBorderColor = "#555555"
+    , keys = customKeys <+> keys defaultConfig
+    , layoutHook = layouts }
+
