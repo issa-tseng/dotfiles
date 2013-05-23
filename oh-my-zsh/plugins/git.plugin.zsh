1d889eeca77aade1c225915a70f9cdc8e72f7060
Clint Tseng <clint@dontexplain.com>
2013-05-22 21:58:46 -0700

    ash: kill oh-my-zsh's default git aliases.

--- oh-my-zsh/plugins/git.plugin.zsh
+++ oh-my-zsh/plugins/git.plugin.zsh
@@ -1,19 +1,3 @@
-# Aliases
-alias g='git'
-alias gst='git status'
-alias gl='git pull'
-alias gup='git fetch && git rebase'
-alias gp='git push'
-alias gd='git diff | mate'
-alias gdv='git diff -w "$@" | vim -R -'
-alias gc='git commit -v'
-alias gca='git commit -v -a'
-alias gb='git branch'
-alias gba='git branch -a'
-alias gcount='git shortlog -sn'
-alias gcp='git cherry-pick'
-
-
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
 
